09bcde11e9301a411f7443a8cd60d902ddd88b62
Václav Pelíšek <peldax@gmail.com>
2018-10-23 15:19:38 +0200

    Create Generator.cpp

---
+++ src/Generator.cpp
@@ -0,0 +1,19 @@
+/*                                                                          */
+/* Copyright (c) 2018 Václav Pelíšek (info@peldax.com)                      */
+/* This file is part of Forester.                                           */
+/*                                                                          */
+/* Forester is free software: you can redistribute it and/or modify         */
+/* it under the terms of the GNU General Public License as published by     */
+/* the Free Software Foundation, either version 3 of the License, or        */
+/* (at your option) any later version.                                      */
+/*                                                                          */
+/* Forester is distributed in the hope that it will be useful,              */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU General Public License for more details.                             */
+/*                                                                          */
+/* You should have received a copy of the GNU General Public License        */
+/* along with Forester.  If not, see <http://www.gnu.org/licenses/>.        */
+/*                                                                          */
+
+#include "Generator.hpp"
