46e81511f933f2aaa1f945f284fb0955a7cb1eca
Václav Pelíšek <peldax@gmail.com>
2018-10-23 15:16:26 +0200

    Create SmartGraphSolver.cpp

---
+++ src/SmartGraphSolver.cpp
@@ -0,0 +1,154 @@
+/*                                                                          */
+/* Copyright (c) 2018 Václav Pelíšek (info@peldax.com)                      */
+/* This file is part of Forester.                                           */
+/*                                                                          */
+/* Forester is free software: you can redistribute it and/or modify         */
+/* it under the terms of the GNU General Public License as published by     */
+/* the Free Software Foundation, either version 3 of the License, or        */
+/* (at your option) any later version.                                      */
+/*                                                                          */
+/* Forester is distributed in the hope that it will be useful,              */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of           */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the            */
+/* GNU General Public License for more details.                             */
+/*                                                                          */
+/* You should have received a copy of the GNU General Public License        */
+/* along with Forester.  If not, see <http://www.gnu.org/licenses/>.        */
+/*                                                                          */
+
+#include "SmartGraphSolver.hpp"
+
+bool SmartGraphSolver::isContinuous() noexcept
+{
+    if (m_continuous == CacheState::UNKNOWN)
+    {
+        const bool result = GraphSolver::isContinuous();
+        m_continuous = result ? CacheState::TRUE : CacheState::FALSE;
+    }
+
+    return m_continuous == CacheState::TRUE;
+}
+
+bool SmartGraphSolver::isLocallyContinuous() noexcept
+{
+    if (m_locallyContinuous == CacheState::UNKNOWN)
+    {
+        const bool result = chartrandPippert1974() || bfLocalContinuity();
+        m_locallyContinuous = result ? CacheState::TRUE : CacheState::FALSE;
+    }
+
+    return m_locallyContinuous == CacheState::TRUE;
+}
+
+int SmartGraphSolver::getNodeContinuity() noexcept
+{
+    if (m_nodeContinuity == -1)
+    {
+        if (!isContinuous())
+        {
+            m_nodeContinuity = 0;
+            return m_nodeContinuity;
+        }
+
+        m_nodeContinuity = 1; // TODO
+    }
+
+    return m_nodeContinuity;
+}
+
+int SmartGraphSolver::getEdgeContinuity() noexcept
+{
+    if (m_edgeContinuity == -1)
+    {
+        if (!isContinuous())
+        {
+            m_edgeContinuity = 0;
+            return m_nodeContinuity;
+        }
+
+        m_edgeContinuity = 1; // TODO
+    }
+
+    return m_edgeContinuity;
+}
+
+bool SmartGraphSolver::isKNodeContinuous(int k) noexcept
+{
+    return getNodeContinuity() >= k;
+}
+
+bool SmartGraphSolver::isKEdgeContinuous(int k) noexcept
+{
+    return getEdgeContinuity() >= k;
+}
+
+int SmartGraphSolver::getCircumference() noexcept
+{
+    if (m_circumference == -1)
+    {
+        if (m_hamiltonCircle == CacheState::TRUE)
+        {
+            m_circumference = GraphSolver::getCircumference();
+            return m_circumference;
+        }
+
+        m_circumference = 0;// TODO
+    }
+
+    return m_circumference;
+}
+
+bool SmartGraphSolver::hasClaw() noexcept
+{
+    if (m_hasClaw == CacheState::UNKNOWN)
+    {
+        const bool result = bfHasClaw();
+        m_hasClaw = result ? CacheState::TRUE : CacheState::FALSE;
+    }
+
+    return m_hasClaw == CacheState::TRUE;
+}
+
+bool SmartGraphSolver::isClaw() noexcept
+{
+    return GraphSolver::isClaw();
+}
+
+bool SmartGraphSolver::isComplete() noexcept
+{
+    return GraphSolver::isComplete();
+}
+
+bool SmartGraphSolver::hasHamiltonRoute() noexcept
+{
+    if (m_hamiltonRoute == CacheState::UNKNOWN)
+    {
+        if (m_hamiltonCircle == CacheState::TRUE)
+        {
+            m_hamiltonRoute = CacheState::TRUE;
+            return true;
+        }
+
+        const bool result = GraphSolver::hasHamiltonRoute();
+        m_hamiltonRoute = result ? CacheState::TRUE : CacheState::FALSE;
+    }
+
+    return m_hamiltonRoute == CacheState::TRUE;
+}
+
+bool SmartGraphSolver::hasHamiltonCircle() noexcept
+{
+    if (m_hamiltonRoute == CacheState::UNKNOWN)
+    {
+        if (m_hamiltonRoute == CacheState::FALSE)
+        {
+            m_hamiltonRoute = CacheState::FALSE;
+            return false;
+        }
+
+        const bool result = GraphSolver::hasHamiltonCircle();
+        m_hamiltonRoute = result ? CacheState::TRUE : CacheState::FALSE;
+    }
+
+    return m_hamiltonRoute == CacheState::TRUE;
+}
